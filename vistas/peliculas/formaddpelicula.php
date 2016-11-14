f2ad8dbaa62aeefb446ebf826b8f714a2bc86f81
admin <admin@PC06>
2016-11-14 12:19:40 +0100

    DWES - 13/11/2016 3º Comienzo Proyecto

---
+++ vistas/peliculas/formaddpelicula.php
@@ -0,0 +1,8 @@
+
+
+<!--crear formulario-->
+<form method="get"  action="index.php">
+  Aqui iria el formulario para añadir una película
+  <input type="hidden" name="do" value="addPelicula"/>
+  <input type="submit"/>
+</form>
