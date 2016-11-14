093f55f24f541d48d6d300606bdedd063e2cfd84
admin <admin@PC06>
2016-11-14 14:26:37 +0100

    DWES - 13/11/2016 7º Comienzo Proyecto Modificacion Y añadido del profesor

--- controlador.php
+++ controlador.php
@@ -12,7 +12,9 @@ include_once ("seguridad.php");
 class Controlador {
 
   /**
-   * Metodo
+   * Metodo controlador que va ejecutando
+   * las acciones que le van llegando desde el formulario
+   * y distintas clases
    */
   public static function control() {
     session_start();
@@ -22,83 +24,81 @@ class Controlador {
     } else {
       $accion = $_REQUEST["do"];
     }
-    
+
     switch ($accion) {
 
-        // ******************** LOGIN *************************
-        case "mostrarFormularioLogin":
-          Vista::show("login/formLogin");
-          break;
+      // ******************** LOGIN *************************
+      case "mostrarFormularioLogin":
+        Vista::show("login/formLogin");
+        break;
 
-        case "procesarFormularioLogin":
-          $loginOk = Login::checkLogin();
-          if ($loginOk) {
-            if (Seguridad::getTipoUsuario() == "admin") {
-              Vista::show("menu/menuAdmin");
-            } 
-            if (Seguridad::getTipoUsuario() == "user") {
-              Vista::show("menu/menuUser");
-            }
-          } else {
-            Vista::show("login/errorLogin");
-          }
-          break;
-          
-          
-        case "mostrarFormularioAltaUsuario":
-          Vista::show("usuarios/formularioAltaUsuario");
-          break;
-
-        case "procesarFormularioAltaUsuario":
-          $result = Usuarios::insertarUsuario();
-          if ($result == 1) {
-              Vista::show("usuarios/insercionOk");
-          }
-          else {
-            Vista::show("usuarios/insercionError");
-          }
-          break;
-        
-        case "cerrarsesion":
-          Seguridad::cerrarSesion();
-          echo "La sesion se ha cerrado correctamente<br/>";
-          echo "<a href='index.php'>Volver al inicio</a>";
-          break;
-        // ******************** MENÚS *************************
-        case "showmenuadmin":
+      case "procesarFormularioLogin":
+        $loginOk = Login::checkLogin();
+        if ($loginOk) {
           if (Seguridad::getTipoUsuario() == "admin") {
             Vista::show("menu/menuAdmin");
-          } else {
-            Vista::show("login/formLogin");
           }
-          break;
-        case "showMenuUser":
           if (Seguridad::getTipoUsuario() == "user") {
             Vista::show("menu/menuUser");
-          } else {
-            Vista::show("login/formLogin");
           }
-          break;
-        // ******************** PELICULAS *************************
-        case "formAnadirPelicula":
-          if (Seguridad::getTipoUsuario() == "admin") {
-            Vista::show("peliculas/formAddpelicula");
-          } else {
-            Vista::show("login/formLogin");
-          }
-          break;
-        case "addPelicula":
-          if (Seguridad::getTipoUsuario() == "admin") {
-            
-          } else {
-            Vista::show("login/formLogin");
-          }
-          break;
+        } else {
+          Vista::show("login/errorLogin");
+        }
+        break;
+
+
+      case "mostrarFormularioAltaUsuario":
+        Vista::show("usuarios/formularioAltaUsuario");
+        break;
+
+      case "procesarFormularioAltaUsuario":
+        $result = Usuarios::insertarUsuario();
+        if ($result == 1) {
+          Vista::show("usuarios/insercionOk");
+        } else {
+          Vista::show("usuarios/insercionError");
+        }
+        break;
+
+      case "cerrarsesion":
+        Seguridad::cerrarSesion();
+        echo "La sesion se ha cerrado correctamente<br/>";
+        echo "<a href='index.php'>Volver al inicio</a>";
+        break;
+      // ******************** MENÚS *************************
+      case "showmenuadmin":
+        if (Seguridad::getTipoUsuario() == "admin") {
+          Vista::show("menu/menuAdmin");
+        } else {
+          Vista::show("login/formLogin");
+        }
+        break;
+      case "showMenuUser":
+        if (Seguridad::getTipoUsuario() == "user") {
+          Vista::show("menu/menuUser");
+        } else {
+          Vista::show("login/formLogin");
+        }
+        break;
+      // ******************** PELICULAS *************************
+      case "formAnadirPelicula":
+        if (Seguridad::getTipoUsuario() == "admin") {
+          Vista::show("peliculas/formAddpelicula");
+        } else {
+          Vista::show("login/formLogin");
+        }
+        break;
+      case "addPelicula":
+        if (Seguridad::getTipoUsuario() == "admin") {
+          
+        } else {
+          Vista::show("login/formLogin");
+        }
+        break;
 //          
 //          
 //        ...etc...
-      }
     }
   }
 
-
+}
