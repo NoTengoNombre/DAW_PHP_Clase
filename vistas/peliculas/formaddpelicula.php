658d9337b4dac40da91cf6b31e6d977546fd48df
admin <admin@PC06>
2016-11-16 14:29:03 +0100

    DWES - 14/11/2016 7º Proyecto Arreglando controlador Modificacion Y añadido del profesor

--- vistas/peliculas/formaddpelicula.php
+++ vistas/peliculas/formaddpelicula.php
@@ -1,7 +1,10 @@
 
 
-<!--crear formulario-->
-<form method="get"  action="index.php">
+<!-- Crear formulario 
+Lo envia al index para realizar la inserccion
+tiene que estar en la parte de administrador
+-->
+<form method="get" action="index.php">
   Aqui iria el formulario para añadir una película
   <input type="hidden" name="do" value="addPelicula"/>
   <input type="submit"/>
