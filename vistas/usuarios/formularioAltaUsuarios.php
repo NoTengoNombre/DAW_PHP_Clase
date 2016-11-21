8e5b2abc84db7d4366b43c94761956e84b4d1b83
admin <admin@PC06>
2016-11-21 12:34:05 +0100

    Ejercicios DIW JQUERY 3

--- vistas/usuarios/formularioAltaUsuarios.php
+++ vistas/usuarios/formularioAltaUsuarios.php
@@ -1,12 +1,51 @@
 <h3>Alta de usuario</h3>
-<form action="index.php" method="GET">
-  Usuario:
-  <input type="text" name="usuario" />
-  <br/>
-  Contraseña:
-  <input type="text" name="passwd" />
-  <br/>
-  Aqui van el resto de campos del formulario de alta de usuarios <br/>
+<form action="index.php" method="get">
+  <label>  
+    Nombre Usuario:
+  </label>
+  <input type="text" name="usuario" value=""/>
+  <br>
+  <label>
+    Fecha Nacimiento :
+  </label>
+  <input type="text" name="" value="" />
+  <br>
+  <label>
+    Ciudad : 
+  </label>
+  <input type="text" name="" value="" />
+  <br>
+  <label>
+    Genero : 
+  </label>
+  <input type="text" name="" value="" />
+  <br>
+  <label>
+    Imagen Usuario :
+  </label>
+  <input type="text" name="" value="" />
+  <br>
+  <label>
+    Email :
+  </label>
+  <input type="text" name="" value="" />
+  <br>
+  <label>
+    Biografia : 
+  </label>
+  <input type="text" name="" value="" />
+  <br>
+  <label>
+    Contraseña:
+  </label>
+  <input type="text" name="passwd" value=""/>
+  <br>
+  <label>
+    Tipo Usuario :
+  </label>
+  <input type="text" name="" value="" />
+  <!-- Aqui van el resto de campos del formulario de alta de usuarios <br/> -->
   <input type="hidden" name="do" value="procesarFormularioAltaUsuario"/>
+  <br><br>
   <input type="submit"/>
 </form>
